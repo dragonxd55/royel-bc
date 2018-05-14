a3e6b1817d93eec6be33f2afd65869f9c54a078e
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-14 21:15:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 
 
     client.on('message' , message => {
-      var prefix = "#";
+      var prefix = "-";
       if(message.author.bot) return;
     
       if(message.content.startsWith(prefix + "bcrole")) {
@@ -109,7 +109,7 @@ client.on('message', message => {
 
           client.on('message', message => {
             if(!message.channel.guild) return;
-  var prefix = "#";
+  var prefix = "-";
   if(message.content.startsWith(prefix + 'bc')) {
   if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
